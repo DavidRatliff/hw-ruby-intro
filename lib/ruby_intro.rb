a8e02ee822717a74e3dc78398450aee21f8c3526
EC2 Default User <ec2-user@ip-172-31-1-46.us-east-2.compute.internal>
2020-01-27 22:54:08 +0000

    completed part 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -17,12 +17,24 @@ end
 # Part 2
 
 def hello(name)
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
+  if s.length == 0 or s[0].capitalize == s[0].downcase
+    return false
+  end
+  return !(%w(A E I O U).include? s[0].capitalize)
 end
 
 def binary_multiple_of_4? s
+  if s.split('').all? { |char| %w(1 0).include? char }
+    if s[-2, 2].eql? '00' or s.eql? '0'
+      return true
+    end
+  end
+  
+  return false
 end
 
 # Part 3
