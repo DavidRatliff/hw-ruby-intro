e96857fc33c7d5b652072973c3e082d64bb2a91a
EC2 Default User <ec2-user@ip-172-31-1-46.us-east-2.compute.internal>
2020-01-27 22:53:56 +0000

    completed part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,29 +3,26 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return arr.max(2).sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.combination(2).any? { |pair| pair.sum == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
 end
 
 # Part 3
